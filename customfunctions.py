40d0f7eb774b046b3fa3406b7b782ea1e0ab5d63
naftalima <npl@cin.ufpe.br>
2020-09-16 13:29:40 -0300

    loc networks with most title

--- customfunctions.py
+++ customfunctions.py
@@ -1,2 +1,13 @@
 import pandas as pd
 import numpy as np
+import re
+
+def functionnetworks_more_titlename(df_tt, networks): 
+    column_names = list(df_tt.columns)
+    df_most_popular = pd.DataFrame(columns = column_names)
+    for i in networks:
+        df = df_tt[df_tt['network'].str.contains(i, flags=re.IGNORECASE, regex=True, na=False)]
+        df_most_popular = df_most_popular.append(df,ignore_index=True)
+    return df_most_popular
+
+# df_tvtime_amazon['name'].describe() # NÃO TA DUPLICADO
